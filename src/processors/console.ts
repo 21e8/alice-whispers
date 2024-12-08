cba42e06ad061dd9ea2aa177299876932eca5157
21e8 <alize_in_cryptoland@protonmail.com>
2024-12-08 21:59:55 +0100

    remove dep

--- src/processors/console.ts
+++ src/processors/console.ts
@@ -1,18 +1,17 @@
 import { Message, MessageProcessor } from '../types';
-import chalk from 'chalk';
 
 export function createConsoleProcessor(): MessageProcessor {
   async function processBatch(messages: Message[]): Promise<void> {
     for (const msg of messages) {
       switch (msg[2]) {
         case 'info':
-          console.log(chalk.blue('ℹ'), msg[1]);
+          console.log('ℹ', msg[1]);
           break;
         case 'warning':
-          console.log(chalk.yellow('⚠'), msg[1]);
+          console.log('⚠', msg[1]);
           break;
         case 'error':
-          console.log(chalk.red('🚨'), msg[1], msg[3]);
+          console.log('🚨', msg[1], msg[3]);
           break;
       }
     }
