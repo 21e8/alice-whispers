63c2d123846f7fb95abe39f31cbb2b8b12aec266
21e8 <alize_in_cryptoland@protonmail.com>
2024-12-08 18:58:29 +0100

    fix

--- src/processors/console.ts
+++ src/processors/console.ts
@@ -4,19 +4,19 @@ import chalk from 'chalk';
 export function createConsoleProcessor(): MessageProcessor {
   async function processBatch(messages: Message[]): Promise<void> {
     for (const msg of messages) {
-      switch (msg.level) {
+      switch (msg[2]) {
         case 'info':
-          console.log(chalk.blue('ℹ'), msg.text);
+          console.log(chalk.blue('ℹ'), msg[1]);
           break;
         case 'warning':
-          console.log(chalk.yellow('⚠'), msg.text);
+          console.log(chalk.yellow('⚠'), msg[1]);
           break;
         case 'error':
-          console.log(chalk.red('🚨'), msg.text);
+          console.log(chalk.red('🚨'), msg[1], msg[3]);
           break;
       }
     }
   }
 
-  return { processBatch, name: 'console' };
+  return { type: 'external', processBatch, name: 'console' };
 }
